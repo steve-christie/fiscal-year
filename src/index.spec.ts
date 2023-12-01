29211abccc92859259eeb14270f9cacb9982f555
steve-christie <steven.christie@hotmail.co.uk>
2023-12-01 11:08:47 +0000

    Added husky, prettier, istanbul

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -47,12 +47,3 @@ describe("getFiscalYearOfDate Tests", () => {
         })
     })
 })
-
-describe("Fiscal Year Tests",  () => {
-
-
-    it ("When requested, the FY Start month can be returned", () => {
-        const fy = fiscalYear(2023, 3)
-        expect(fy.getStartMonthIndex()).to.eq(3)
-    })
-});
